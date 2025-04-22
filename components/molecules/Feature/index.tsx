1156abea6a2e16eb68bd8f560ea013b3ec7fd574
striker1826 <alstjq123579@daum.net>
2025-04-22 19:04:20 +0900

    fix: 1차 QA - UI 수정

--- components/molecules/Feature/index.tsx
+++ components/molecules/Feature/index.tsx
@@ -16,11 +16,12 @@ const Feature = ({ mainText, subText, children }: FeatureProps) => {
   return (
     <motion.div
       ref={ref}
+      className="w-full"
       initial={{ opacity: 0 }}
       animate={{ opacity: isVisible ? 1 : 0 }}
       transition={{ duration: 1, ease: "easeInOut" }}
     >
-      <p className="text-center text-purple text-[16px] md:text-[18px] lg:text-[20px] font-[700] leading-[128%] tracking-[-0.48px]">
+      <p className="w-full text-center text-purple text-[16px] md:text-[18px] lg:text-[20px] font-[700] leading-[128%] tracking-[-0.48px]">
         {subText}
       </p>
       <p className="mt-[16px] sm:mt-[20px] md:mt-[24px] lg:mt-[26px] text-center text-black text-[26px] sm:text-[32px] md:text-[40px] lg:text-[48px] font-[700] leading-[136%] tracking-[-0.78px]">
--- components/organisms/FeatureEgg/index.tsx
+++ components/organisms/FeatureEgg/index.tsx
@@ -3,12 +3,13 @@
 import landingEgg from "@/public/landing_egg.json";
 import Feature from "@/components/molecules/Feature";
 import dynamic from "next/dynamic";
+import Image from "next/image";
 
 const FeatureEgg = () => {
   const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
   return (
-    <div className="flex flex-col bg-[#fff] items-center justify-center pb-[80px] sm:pb-[128px] md:pb-[176px]">
+    <div className="flex flex-col px-[30px] bg-[#fff] items-center justify-center pb-[80px] sm:pb-[128px] md:pb-[176px]">
       <Feature
         mainText={
           <>
@@ -19,10 +20,32 @@ const FeatureEgg = () => {
         }
         subText="행운의 오리알"
       >
-        <Lottie
-          className="mt-[56px] w-[200px] h-[440px] sm:w-[280px] sm:h-[616px] md:w-[320px] md:h-[705px] lg:w-[360px] lg:h-[792px]"
-          animationData={landingEgg}
-        />
+        <div className="relative max-w-[1369px] w-full flex justify-center items-center">
+          <Lottie
+            className="mt-[56px] w-[200px] w-full h-[440px] sm:w-[280px] sm:h-[616px] md:w-[320px] md:h-[705px] lg:w-[360px] lg:h-[792px]"
+            animationData={landingEgg}
+          />
+          <Image
+            src={"/images/featureEgg/confetti.svg"}
+            className="w-full"
+            fill
+            alt="confetti"
+          />
+          <Image
+            className="absolute top-[283px] left-[8px]"
+            src={"/images/featureEgg/leftCoin.svg"}
+            width={100}
+            height={100}
+            alt="coin"
+          />
+          <Image
+            className="absolute top-[140px] right-[8px]"
+            src={"/images/featureEgg/rightCoin.svg"}
+            width={100}
+            height={100}
+            alt="coin"
+          />
+        </div>
       </Feature>
     </div>
   );
